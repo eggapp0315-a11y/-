94a6f38a6a75b5a4fa42a62be75e7ca7e5790a8d
kali <eggapp0315@gmail.com>
2026-01-29 15:34:50 -0800

    update project

--- app.py
+++ app.py
@@ -338,3 +338,11 @@ if __name__ == "__main__":
      with app.app_context():
          db.create_all()
      app.run(debug=False)
+
+
+
+
+#上傳
+#git add .
+#git commit -m "update project"
+#git push
