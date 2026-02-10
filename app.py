8a7dc602b13e321c533ffc48f9957ccb0482ea8f
kali <eggapp0315@gmail.com>
2026-02-09 20:22:58 -0800

    update project

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def allowed_file(filename):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(225), nullable=False)
     role = db.Column(db.String(20), default="student")  # student / admin
 
     def set_password(self, pw):
